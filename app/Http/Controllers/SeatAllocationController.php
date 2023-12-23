093fd3595e03c5097065d4d7ff1cfea5c5d5edd0
engsiam <siam.softwareengineer@gmail.com>
2023-12-23 21:56:01 +0600

    add

--- app/Http/Controllers/SeatAllocationController.php
+++ app/Http/Controllers/SeatAllocationController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SeatAllocation;
 use Illuminate\Http\Request;
 
 class SeatAllocationController extends Controller
 {
     public function index(){
-        
+        $seat = SeatAllocation::with('bus')->get();
+        return $seat;
     }
 }
